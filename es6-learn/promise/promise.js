ccad35a638a251e8034bf68531650c16f640d8a9
jiajun.qin <jiajun.qin@brgroup.com>
2020-08-06 20:47:05 +0800

    up

--- es6-learn/promise/promise.js
+++ es6-learn/promise/promise.js
@@ -1,14 +1,11 @@
-const p1 = new Promise(function (resolve, reject) {
-  setTimeout(() => reject(new Error('fail')), 3000);
-});
-
-const p2 = new Promise(function (resolve, reject) {
-  setTimeout(() => resolve(p1), 1000);
-});
-
-console.log(p2.then(null, (err) => {
-  console.log(err);
-  return p1.constructor;
-}));
-
-// console.log(p1, p1.then().then());
+// finally 的实现
+Promise.prototype.finally = function (callback) {
+  let P = this.constructor;
+  return this.then(
+    (value) => P.resolve(callback()).then(() => value),
+    (reason) =>
+      P.resolve(callback()).then(() => {
+        throw reason;
+      })
+  );
+};
